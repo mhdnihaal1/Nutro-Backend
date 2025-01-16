f808c750495488e7958640f12637a987923c1902
mhdnihaal1 <nihalmuhaednihal@gmail.com>
2025-01-16 14:07:06 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { httpServer } from "./infrastructureLayer/config/app";
 import { connectDB } from "./infrastructureLayer/config/connect-DB";
 
-const PORT = process.env.MYPORT;
-
+const PORT = process.env.MYPORT || 8000
+console.log(PORT)
 const startServer = async (): Promise<void> => {
   await connectDB();
   const app = httpServer;
