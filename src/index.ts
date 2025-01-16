666f10b4fdcad53daf3cb3a9583dd220bba764a2
mhdnihaal1 <nihalmuhaednihal@gmail.com>
2025-01-16 12:35:32 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,8 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin:"*",
+        origin: "http://localhost:3000", // Specify the frontend URL
+        credentials: true, // Allow credentials (cookies, tokens)
     })
 )
 
