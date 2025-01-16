65d88ff27fcab478c57c30adf48118a50feee738
mhdnihaal1 <nihalmuhaednihal@gmail.com>
2025-01-16 13:57:07 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,14 @@
+import { httpServer } from "./infrastructureLayer/config/app";
+import { connectDB } from "./infrastructureLayer/config/connect-DB";
 
-// const config = require('./config.json')
+const PORT = process.env.MYPORT;
 
+const startServer = async (): Promise<void> => {
+  await connectDB();
+  const app = httpServer;
+  const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-// mongoose.connect(config.connectionString)
-
-// const User = require('./model/user_model')
-// const Note = require('./model/note_model')
- 
-import express, { Request, Response } from 'express';
-const cors = require('cors');
-const app = express()
-const mongoose = require('mongoose')
-require('dotenv').config();
-
-// const jwt = require('jsonwebtoken');
-// const { authenticateToken } = require('./utilities')
-
-import UserRoute from './infrastructureLayer/router/user-route'
-import AdminRoute from './infrastructureLayer/router/admin-route'
-import AgentRoute from './infrastructureLayer/router/agent-route'
-
-app.use(express.json())
-
-app.use(
-    cors({
-        origin: process.env.PORT, // Specify the frontend URL
-        credentials: true, // Allow credentials (cookies, tokens)
-    })
-)
-
-app.get('/',(req,res)=>{
-  
-    res.send('This is Nutro backend:Nutro Backend is Running')
-    
-  })
-
-
-app.use("/api/user", UserRoute);
-
-app.use("/api/admin", AdminRoute);
-
-app.use("/api/agent", AgentRoute);
-
-
-app.listen(8000,()=>{
-    console.log("Server is running....")
-})
-
-module.exports = app ;
+startServer();
