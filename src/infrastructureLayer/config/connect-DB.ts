ff9ac9e8372e337032ddb81e147a365f648d82d3
mhdnihaal1 <nihalmuhaednihal@gmail.com>
2025-09-19 10:18:27 +0530

    first commit

--- src/infrastructureLayer/config/connect-DB.ts
+++ src/infrastructureLayer/config/connect-DB.ts
@@ -4,8 +4,8 @@ import { DB_NAME } from "./constant";
 const connectDB = async () => {
   try {
     const mongo_uri =
-      process.env.MONGODB_URI 
-// "mongodb+srv://mhdnihal:12345@cluster1.jpsjsgj.mongodb.net/Nutro-App?retryWrites=true&w=majority&appName=Cluster1"
+      process.env.MONGODB_URI ||
+"mongodb+srv://mhdnihal:12345@cluster1.jpsjsgj.mongodb.net/Nutro-App?retryWrites=true&w=majority&appName=Cluster1"
     if (mongo_uri) {
       await mongoose.connect(mongo_uri);
     }
