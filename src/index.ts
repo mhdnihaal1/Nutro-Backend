2f7f8af921c51723fcfae9c82c03d17e8df4e955
mhdnihaal1 <nihalmuhaednihal@gmail.com>
2025-09-19 12:06:49 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import app,{ httpServer } from "./infrastructureLayer/config/app";
+import app, { httpServer } from "./infrastructureLayer/config/app";
 import { connectDB } from "./infrastructureLayer/config/connect-DB";
 
-const MYPORT = process.env.MYPORT ;
+const PORT = process.env.PORT || process.env.MYPORT || 8000; // <-- use Render's PORT
 
 const startServer = async (): Promise<void> => {
-  await connectDB(); 
-  const app = httpServer;
-  app.listen(MYPORT, () => {
-    console.log(`Server running on port ${MYPORT}`);
+  await connectDB();
+  httpServer.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 };
 
